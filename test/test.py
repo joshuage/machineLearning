05dad6603f094fbc065b09409ddf4ea86a1ba61e
Joshua <joshua962619@gmail.com>
2020-11-12 11:43:48 -0700

    iris

---
+++ test/test.py
@@ -0,0 +1,16 @@
+import tensorflow as tf
+
+# tensorflow_version = tf.__version__
+#
+# gpu_available = tf.test.is_gpu_available()
+# print('version', tensorflow_version, 'gpu', gpu_available)
+#
+# a = tf.constant([1.0,2.0], name='a')
+# b = tf.constant([1.0,2.0], name='b')
+#
+# result = tf.add(a, b, name='add')
+#
+# print(result)
+
+r = 'adfasfsaf'
+print(r[:-2])
